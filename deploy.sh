e66e94a89cfef273dc9e1d67ed3f6b2ef38d9a9b
Santiago E <santiago93echevarria@gmail.com>
2024-02-18 20:35:32 -0300

    version script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+while getopts v: opt; do
+  case ${opt} in
+    v) VERSION=${OPTARG};;
+  esac
+done 
+
+sed -i "s/__version__ = \".*\"/__version__ = \"$VERSION\"/g" $(dirname $0)/src/sutils/__init__.py
+
 rm -rf $(dirname $0)/dist
 rm -rf $(dirname $0)/src/utils_esantix.egg-info
-
 
 # python3 -m pip install --upgrade build
 python3 -m build
