b4b45b05c99e7f3830e96b0cd3897b6b5d61a2d7
Santiago E <santiago93echevarria@gmail.com>
2024-02-20 11:45:29 -0300

    awk instead of grep

--- scripts/package-diff.sh
+++ scripts/package-diff.sh
@@ -3,11 +3,10 @@
 src_path=$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )/src
 
 git fetch --all
-changed_files=$( git diff HEAD --name-only | grep 'src/\|pyproject.toml\|requirements.txt') # TODO: ignore whitespaces, lines, etcf
+changed_files=$( git diff HEAD --name-only | awk '/^src\//') # TODO: ignore whitespaces, lines, etcf
 
 if [ -z $changed_files ]; then
     echo "No package files changed"
-    exit 0
 else
     echo "Detected changes in: $changed_files"
 fi
