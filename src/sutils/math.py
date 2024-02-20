68293c0bdba82d8fe829cc0cd456a7fbcdfd11f5
Santiago E <santiago93echevarria@gmail.com>
2024-02-20 13:26:48 -0300

    change package code example

---
+++ src/sutils/math.py
@@ -0,0 +1,8 @@
+
+def increase(a, amount=1):
+    """Increase number by certain amount"""
+    return a+amount
+
+def decrease(a, amount=1):
+    """Decrease number by certain amount"""
+    return a-amount
